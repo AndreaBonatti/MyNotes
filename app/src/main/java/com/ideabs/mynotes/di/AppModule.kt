e1a5fb3e469c03c86139e50c0f837f257f20b9a0
Andrea Bonatti <andreabonatti92@gmail.com>
2025-05-21 12:17:56 +0200

    Update the local api url

--- app/src/main/java/com/ideabs/mynotes/di/AppModule.kt
+++ app/src/main/java/com/ideabs/mynotes/di/AppModule.kt
@@ -24,7 +24,7 @@ val appModule = module {
     single<ApiRepository> {
         RemoteApiRepository(
             client = get(),
-            baseUrl = "192.168.0.56" //TODO insert the api entrypoint
+            baseUrl = "192.168.0.56:8000" //TODO insert the api entrypoint
         )
     }
 
