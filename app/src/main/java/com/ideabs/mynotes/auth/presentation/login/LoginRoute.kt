98591e635980f2ed12212b62f22c4345d204ebf5
Andrea Bonatti <andreabonatti92@gmail.com>
2025-05-22 16:46:26 +0200

    Added a reset of the login view model state if the user click on the register button in the login screen

--- app/src/main/java/com/ideabs/mynotes/auth/presentation/login/LoginRoute.kt
+++ app/src/main/java/com/ideabs/mynotes/auth/presentation/login/LoginRoute.kt
@@ -18,6 +18,7 @@ fun LoginRoute(
     LoginScreen(
         viewModel = viewModel,
         onNavigateToRegister = {
+            viewModel.resetState()
             navController.navigate("register")
         }
     )
