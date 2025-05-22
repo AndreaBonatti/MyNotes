ac8bbeb2e2d36bd1fa8f8547bbba3a586fee513d
Andrea Bonatti <andreabonatti92@gmail.com>
2025-05-22 11:27:59 +0200

    Added the proper library to allow ktor to log the API I/O

--- app/src/main/java/com/ideabs/mynotes/di/AppModule.kt
+++ app/src/main/java/com/ideabs/mynotes/di/AppModule.kt
@@ -7,6 +7,13 @@ import com.ideabs.mynotes.core.data.RemoteApiRepository
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.ANDROID
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.core.module.dsl.viewModel
@@ -16,8 +23,15 @@ val appModule = module {
 
     single {
         HttpClient(CIO) {
+            install(Logging) {
+                level = LogLevel.ALL
+                logger = Logger.ANDROID
+            }
             install(ContentNegotiation) {
                 json(Json { ignoreUnknownKeys = true })
+            }
+            defaultRequest {
+                contentType(ContentType.Application.Json)
             }
         }
     }
