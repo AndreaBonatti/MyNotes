b2a55369ae0f01747230a27c446687e836b49227
Andrea Bonatti <andreabonatti92@gmail.com>
2025-05-23 16:09:20 +0200

    Extract the common JWT token functions inside the JwtUtils.kt file

--- app/src/main/java/com/andreabonatti92/mynotes/auth/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/andreabonatti92/mynotes/auth/presentation/login/LoginViewModel.kt
@@ -4,11 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.andreabonatti92.mynotes.core.data.ApiRepository
 import com.andreabonatti92.mynotes.core.data.UserPreferences
+import com.andreabonatti92.mynotes.core.domain.isValidJwt
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import java.util.Base64
 
 class LoginViewModel(
     private val apiRepository: ApiRepository,
@@ -51,40 +51,6 @@ class LoginViewModel(
 
     fun resetState() {
         _loginState.value = LoginState.Idle
-    }
-
-    private fun isValidJwt(token: String): Boolean {
-        return token.count { it == '.' } == 2 && !isJwtExpired(token)
-    }
-
-    private fun isJwtExpired(token: String): Boolean {
-        return try {
-            val payloadJson = decodeJwtPayload(token)
-            val exp = payloadJson["exp"]?.toLongOrNull()
-            if (exp != null) {
-                val currentTimeSec = System.currentTimeMillis() / 1000
-                exp < currentTimeSec // expired if exp is in the past
-            } else {
-                true // no exp claim? treat as expired
-            }
-        } catch (e: Exception) {
-            true // any parse failure = invalid/expired
-        }
-    }
-
-    private fun decodeJwtPayload(token: String): Map<String, String> {
-        val parts = token.split(".")
-        if (parts.size != 3) throw IllegalArgumentException("Invalid JWT structure")
-
-        val payloadBase64 = parts[1]
-        val decoded = Base64.getUrlDecoder().decode(payloadBase64)
-        val json = String(decoded, Charsets.UTF_8)
-
-        val regex = "\"(\\w+)\":\\s*\"?(\\d+|[^\"]+)\"?".toRegex()
-        return regex.findAll(json).associate { matchResult ->
-            val (key, value) = matchResult.destructured
-            key to value
-        }
     }
 }
 
---
+++ app/src/main/java/com/andreabonatti92/mynotes/core/domain/JwtUtils.kt
@@ -0,0 +1,37 @@
+package com.andreabonatti92.mynotes.core.domain
+
+import java.util.Base64
+
+public fun isValidJwt(token: String): Boolean {
+    return token.count { it == '.' } == 2 && !isJwtExpired(token)
+}
+
+private fun isJwtExpired(token: String): Boolean {
+    return try {
+        val payloadJson = decodeJwtPayload(token)
+        val exp = payloadJson["exp"]?.toLongOrNull()
+        if (exp != null) {
+            val currentTimeSec = System.currentTimeMillis() / 1000
+            exp < currentTimeSec // expired if exp is in the past
+        } else {
+            true // no exp claim? treat as expired
+        }
+    } catch (e: Exception) {
+        true // any parse failure = invalid/expired
+    }
+}
+
+private fun decodeJwtPayload(token: String): Map<String, String> {
+    val parts = token.split(".")
+    if (parts.size != 3) throw IllegalArgumentException("Invalid JWT structure")
+
+    val payloadBase64 = parts[1]
+    val decoded = Base64.getUrlDecoder().decode(payloadBase64)
+    val json = String(decoded, Charsets.UTF_8)
+
+    val regex = "\"(\\w+)\":\\s*\"?(\\d+|[^\"]+)\"?".toRegex()
+    return regex.findAll(json).associate { matchResult ->
+        val (key, value) = matchResult.destructured
+        key to value
+    }
+}
